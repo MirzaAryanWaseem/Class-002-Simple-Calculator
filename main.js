1d0447cc762955c507789d14a3a45191c1890543
MirzaAryanWaseem <aryanbaig293@gmail.com>
2024-03-21 22:57:35 +0500

    updated

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const inquirer_1 = __importDefault(require("inquirer"));
-const answer = await inquirer_1.default.prompt([
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
     { message: "firstNumber", type: "number", name: "firstNumber" },
     { message: "secondNumber", type: "number", name: "secondNumber" },
     {
